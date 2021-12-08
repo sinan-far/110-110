4c2cffdb672dcdba5c7c3eaabb48b3e5c05361b3
sinan-far <81846209+sinan-far@users.noreply.github.com>
2021-12-07 18:35:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var prediction_1 = "";
+var prediction_2 = "";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+console.log("ml5 version",ml5.version);
+
+classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/jrHoMs3h5/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model has been loaded");
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "the first prediction is " + prediction_1;
+    speak_data_2 = "the second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
